32b3d79d2351000150d42d5e2567ab466e53e6f7
LucaPereyra98 <114114184+LucaPereyra98@users.noreply.github.com>
2023-05-01 11:32:40 -0300

    Actualizacion de Desafio Entregable 4

--- DesafioEntregable4/src/app.js
+++ DesafioEntregable4/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const productRouter = require('./routes/products.routes')
 const cartsRouter = require('./routes/carts.routes')
+const viewsRouter = require('./routes/views.routes')
 const open = require("open")
 const morgan = require('morgan')
 const { engine } = require('express-handlebars')
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(`${__dirname}/public`))
 app.use('/api', productRouter)
 app.use('/api', cartsRouter)
+app.use('/', viewsRouter)
 app.use(morgan('dev'))
 app.engine('handlebars', engine())
 app.set('views', __dirname + '/views')
@@ -23,13 +25,3 @@ app.listen(PORT, () => {
   open(`http://localhost:${PORT}`)
 })
 
-app.get('/', (req, res) => {
-  const users = [
-    {name: "Luca", lastName: "Pereyra", age: 25, email: "example1@example.com", phoneNumber: 1122334455},
-    {name: "Tomas", lastName: "Pereyra", age: 23, email: "example2@example.com", phoneNumber: 1122334455},
-    {name: "Maia", lastName: "Pereyra", age: 5, email: "example3@example.com", phoneNumber: 1122334455},
-    {name: "Noah", lastName: "Pereyra", age: 5, email: "example4@example.com", phoneNumber: 1122334455},
-    {name: "Tito", lastName: "Pereyra", age: 58, email: "example5@example.com", phoneNumber: 1122334455}
-  ]
-  res.render('index', users[Math.floor(Math.random()*5)])
-} )
